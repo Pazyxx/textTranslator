6762467a955d86f4cac7d5a95104f33debc42707
Pazyxx <sankese@gmx.de>
2023-07-26 20:26:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transcript: '',
+      started: '',
+      same: false
+    };
+    this.recognition = null;
+  }
+
+  componentDidMount() {
+    // Überprüfe, ob die Web Speech API in Chrome verfügbar ist
+    if ('webkitSpeechRecognition' in window) {
+      // Erstelle eine neue Instanz der Web Speech API
+      this.recognition = new window.webkitSpeechRecognition();
+      this.recognition.continuous = true; // Kontinuierliche Spracherkennung
+
+      // Eventlistener für Spracherkennungsergebnisse
+      this.recognition.onresult = (event) => {
+        const result = event.results[event.results.length - 1][0].transcript;
+        this.setState(prevState => ({
+          transcript: prevState.same ? prevState.transcript : result,
+          same: prevState.transcript === result
+        }));
+      };
+    } else {
+      console.log('Die Web Speech API wird in diesem Browser nicht unterstützt.');
+    }
+  }
+
+  startSpeechRecognition = () => {
+    if (this.recognition) {
+      this.setState({ started: true });
+      this.recognition.start();
+    }
+  };
+
+  stopSpeechRecognition = () => {
+    if (this.recognition) {
+      this.setState({ started: false });
+      this.recognition.stop();
+    }
+  };
+
+  render() {
+    const { transcript, started, same } = this.state;
+
+    return (
+      <section className='main'>
+        <div className='option'>
+          <button id="start" disabled={started} onClick={this.startSpeechRecognition}>START</button>
+          <button id="stop" disabled={!started} onClick={this.stopSpeechRecognition}>STOP</button>
+        </div>
+        <div className='outputbox'>
+          <span id="erkanntertext">TEXT-TRANSLATION</span>
+          <span className='outputtext'>
+            {started && same ? '' : transcript}
+          </span>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;
